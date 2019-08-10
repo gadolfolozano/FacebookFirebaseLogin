ba4090cd37bacbcaeca6396cd9784136d6de7aa8
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2019-08-10 14:50:32 -0500

    initial commit

---
+++ app/src/main/java/pe/com/gadolfolozano/firebasefacebooklogin/MainActivity.java
@@ -0,0 +1,13 @@
+package pe.com.gadolfolozano.firebasefacebooklogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
