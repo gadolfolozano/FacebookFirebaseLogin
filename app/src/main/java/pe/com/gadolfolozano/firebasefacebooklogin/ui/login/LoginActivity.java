1bd5d53f5e0e02f61f542e1b18178580f0381232
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2019-08-11 00:18:01 -0500

    basic facebook login

--- app/src/main/java/pe/com/gadolfolozano/firebasefacebooklogin/ui/login/LoginActivity.java
+++ app/src/main/java/pe/com/gadolfolozano/firebasefacebooklogin/ui/login/LoginActivity.java
@@ -1,6 +1,18 @@
 package pe.com.gadolfolozano.firebasefacebooklogin.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
 
 import javax.inject.Inject;
 
@@ -10,6 +22,9 @@ import pe.com.gadolfolozano.firebasefacebooklogin.base.BaseActivity;
 import pe.com.gadolfolozano.firebasefacebooklogin.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewModel> {
+    private static final String TAG = LoginActivity.class.getCanonicalName();
+
+    private CallbackManager callbackManager;
 
     @Inject
     LoginViewModel viewModel;
@@ -32,6 +47,43 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewM
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        Log.d(TAG, "facebook:onCreate:");
+
+        viewModel.getLoginClick().observe(this, this::observeLoginClick);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // App code
+            }
+        });
+    }
+
+    private void observeLoginClick(Boolean shouldClickLogin) {
+        if (shouldClickLogin != null && shouldClickLogin) {
+            Log.d(TAG, "facebook:performlogin:");
+            LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile", "user_friends"));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/pe/com/gadolfolozano/firebasefacebooklogin/ui/login/LoginViewModel.java
+++ app/src/main/java/pe/com/gadolfolozano/firebasefacebooklogin/ui/login/LoginViewModel.java
@@ -1,13 +1,29 @@
 package pe.com.gadolfolozano.firebasefacebooklogin.ui.login;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
 
+import pe.com.gadolfolozano.firebasefacebooklogin.data.DataManager;
+
 public class LoginViewModel extends ViewModel {
+    private DataManager dataManager;
+
+    private MutableLiveData<Boolean> loginClick;
 
     @Inject
-    public LoginViewModel(){
+    public LoginViewModel(DataManager dataManager) {
+        this.dataManager = dataManager;
+        this.loginClick = new MutableLiveData<>();
+        this.loginClick.setValue(false);
+    }
 
+    public void requestFacebookLogin() {
+        this.loginClick.setValue(true);
+    }
+
+    public MutableLiveData<Boolean> getLoginClick() {
+        return loginClick;
     }
 }
